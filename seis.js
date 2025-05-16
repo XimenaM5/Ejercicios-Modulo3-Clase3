2b18ccea1efe842b36e8120fbf9e0687cd93b1e5
Ximena M <ximenaa4455@gmail.com>
2025-05-15 21:34:35 -0600

    .

--- seis.js
+++ seis.js
@@ -2,13 +2,13 @@
 const tipo = prompt("Ingresa un tipo (A, B, C o D):");
 
 // Evaluación usando escalera else if
-if (tipo === "A") {
-  console.log("Seleccionaste el tipo A");
-} else if (tipo === "B") {
+if (tipo === "A" || tipo == "a") {
+  console.log("Seleccionaste el tipo A" || console.log("Seleccionaste el tipo a"));
+} else if (tipo === "B" || tipo =="b") {
   console.log("Seleccionaste el tipo B");
-} else if (tipo === "C") {
+} else if (tipo === "C" || tipo =="c") {
   console.log("Seleccionaste el tipo C");
-} else if (tipo === "D") {
+} else if (tipo === "D" || tipo =="d") {
   console.log("Seleccionaste el tipo D");
 } else {
   console.log("Selección incorrecta");
